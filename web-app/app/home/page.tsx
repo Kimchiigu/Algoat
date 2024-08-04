b7eb2d4f5f6d94b76e5db9288812ddbb3c59db95
Christopher Hardy Gunawan <christopher.hygunawan@gmail.com>
2024-08-04 20:32:13 +0700

    adding theme button

--- web-app/app/home/page.tsx
+++ web-app/app/home/page.tsx
@@ -11,11 +11,14 @@ import PlayDialog from "../../components/dialog-play";
 import useUserStore from "@/lib/user-store";
 import withAuth from "@/hoc/withAuth";
 import { handleLogout } from "@/controller/user-controller";
+import { useTheme } from "next-themes";
 import Link from "next/link";
 import {
   LogOut,
+  Moon,
   Router,
   Settings,
+  Sun,
   Volume,
   Volume1,
   Volume2,
@@ -29,8 +32,15 @@ import {
 import { useVolume } from "@/lib/VolumeContext";
 import BackgroundAudio from "@/components/background-audio";
 import { useRouter } from "next/navigation";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 function HomePage() {
+  const { setTheme } = useTheme();
   const router = useRouter();
   const { isLoading, fetchUserInfo } = useUserStore();
   const { volume, setVolume } = useVolume();
@@ -148,6 +158,27 @@ function HomePage() {
             <Button variant="ghost" className="p-8">
               <Settings />
             </Button>
+
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" size="icon">
+                  <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                  <span className="sr-only">Toggle theme</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  System
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
 
             <HoverCard>
               <HoverCardTrigger>
